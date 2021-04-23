ae44d9d1f47ee35ef9659f17ef7f7becbee60157
Patrick Nemenz <patrick.nemenz@adesso.at>
2021-04-23 12:32:03 +0200

    ➖(tampermonkey): remove babel dependency

--- src/tampermonkey/userscript.js
+++ src/tampermonkey/userscript.js
@@ -4,8 +4,6 @@
 // @version      0.1
 // @description  Time Reporting!
 // @author       Yourself
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @require      https://cdn.jsdelivr.net/gh/crapkit/timesheet-utils@master/dist/aproda.js
 // @match        https://aproda.adesso.at/arbeitszeit/arbeitszeit.jsf
 // ==/UserScript==
